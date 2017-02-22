e068e224784d325a3c8f90e1bacc2dc5a17937b8
Krzysztof Mazurkiewicz <k3mazurkiewicz@gmail.com>
2017-02-22 20:51:40 +0100

    main + footer

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-$(document).ready(function(){
-
+$(document).ready(
+function(){
+	APP.init();
 });
+
+var APP = (function()
+{
+	let elementToLoadTo = $('main');
+	function getSubpages(url)
+	{
+		elementToLoadTo.slideUp(400, function()
+		{
+			$.ajax({url: url,
+				dataType: 'html'})
+				.done(function (result)
+				{
+					elementToLoadTo.html(result);
+					elementToLoadTo.slideDown();
+				})
+				.fail(function(error)
+				{
+					elementToLoadTo.text(error.statusText);
+				});
+		});
+	}
+
+	return{
+		init: function()
+		{
+			$('nav li').each(function(index){
+				if(index < 3) $(this).on('click', function(){ getSubpages($(this).attr('data-url'));});
+			});
+			getSubpages('partials/home.html');
+		}
+	}
+
+})();
